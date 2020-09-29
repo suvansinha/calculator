cedf436bf6406a998d14fa01de2038fc94c41222
Suvan Sinha <71833649+suvansinha@users.noreply.github.com>
2020-09-29 19:37:28 +0530

    Update multiplier.py

--- multiplier.py
+++ multiplier.py
@@ -1,7 +1,10 @@
-# this program print numbers from 1 to 100 which are divisible by 3
+# this program print multiples of a number less than 100
   
 n = int(input("enter your no. ="))
 
+if n> 100:
+     print("not possible")  
+    
 for i in range(1,101):
    if i*n<= 100:
      print(i*n)
