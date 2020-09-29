26820f290676c68b1a169f39db71b4deb89445c6
Suvan Sinha <71833649+suvansinha@users.noreply.github.com>
2020-09-29 16:23:30 +0530

    Add files via upload

---
+++ multiplier.py
@@ -0,0 +1,15 @@
+# this program print numbers from 1 to 100 which are divisible by 3
+  
+n = int(input("enter your no. ="))
+
+for i in range(1,101):
+   if i*n<= 100:
+     print(i*n)
+    
+    
+    
+   
+    
+       
+
+    
