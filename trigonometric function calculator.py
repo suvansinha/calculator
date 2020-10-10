ef319201b1864cc2fcc17a61f0c63fbcd83e0d30
Suvan Sinha <71833649+suvansinha@users.noreply.github.com>
2020-10-10 12:24:51 +0530

    Add files via upload

---
+++ trigonometric function calculator.py
@@ -0,0 +1,44 @@
+def main():
+    import math
+    trig = input('which trigonometric function you want to calculate? ')
+    if trig == 'sine' or trig == 'sin':
+        a = eval(input('What is the angle measure? '))
+        result = math.sin(math.radians(a))
+        print('The answer is '+str(round(result,3)))
+        main()
+    
+    elif trig == 'cosine' or trig == 'cos':
+        a = eval(input('What is the angle measure? '))
+        result = math.cos(math.radians(a))
+        print('The answer is '+str(round(result,3)))    
+        main()
+        
+    elif trig == 'tangent' or trig == 'tan':
+        a = eval(input('What is the angle measure? '))
+        result = math.tan(math.radians(a))
+        print('The answer is '+str(round(result,3)))    
+        main()    
+        
+    elif trig == 'cotangent' or trig == 'cot':
+        a = eval(input('What is the angle measure? '))
+        result = math.cot(math.radians(a))
+        print('The answer is '+str(round(result,3)))    
+        main()    
+        
+    elif trig == 'secant' or trig == 'sec':
+        a = eval(input('What is the angle measure? '))
+        result = math.sec(math.radians(a))
+        print('The answer is '+str(round(result,3)))    
+        main()  
+        
+    elif trig == 'cosecant' or trig == 'cosec':
+        a = eval(input('What is the angle measure? '))
+        result = math.cosec(math.radians(a))
+        print('The answer is '+str(round(result,3)))    
+        main()    
+        
+    else:
+        print('Sorry wrong input')
+        main()
+        
+main()        
