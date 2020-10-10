f6eee127fc11747fb8213306873ef75d40efa80e
Suvan Sinha <71833649+suvansinha@users.noreply.github.com>
2020-10-10 12:00:16 +0530

    Update multiplier.py

--- multiplier.py
+++ multiplier.py
@@ -1,16 +1,18 @@
-# this program print multiples of a number less than 100
+# this program print multiples of a positive integer less than 100
   
 n = int(input("enter your no. ="))
 
 if n> 100 or n<0:
      print("not possible")
 if n == 0:
-     print("0")     
+     print("0") 
+     
 
     
 for i in range(1,101):
    if 1<=i*n<= 100:
      print(i*n)
+
     
     
     
